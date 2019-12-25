36accc88c041581635b16bba5fbc0421b06735fc
stratospheriquegit config --global user.name stratospheriquegit config --global user.name stratospherique <ahmedosm10@gmail.com>
2019-12-25 13:30:57 +0100

    cors fix #2

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,8 +1,8 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do 
   allow do
-    origins 'http://localhost:3000'
+    #origins 'http://localhost:3000'
     origins 'https://clever-hugle-f515ec.netlify.com'
-    
+
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
