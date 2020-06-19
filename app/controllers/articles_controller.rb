81bca9ecf967a17604f1b8db361fab6db4c4f6d0
stratospherique <ahmedosm10@gmail.com>
2020-06-19 01:35:54 +0100

    add authentication to article creation and favorites + tests

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -31,7 +31,7 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new(article_params)
+    @article = current_user.articles.build(article_params)
     if @article.save
       render json: {
         article: @article
