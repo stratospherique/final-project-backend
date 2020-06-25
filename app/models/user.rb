a109bcb3bab72fb6d5609d01eb1cc0e77b255e09
stratospherique <ahmedosm10@gmail.com>
2020-06-25 02:21:48 +0100

    change the order of user model fields validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,22 +2,21 @@ class User < ApplicationRecord
   # add a default attachment for avatar if empty
   after_commit :attach_avatar, only: [:save]
 
-  has_secure_password
   # for user avatar
   has_one_attached :avatar
-
+  
   has_many :favorites, dependent: :destroy
   has_many :liked_articles, :through => :favorites, class_name: 'Article'
   has_many :articles, dependent: :destroy
-
-
+  
+  
   validates :username, presence: true
   validates :username, uniqueness: true
   validates :username, length: { minimum: 4 }
   validates :email, presence: true
   validates :email, uniqueness: true
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
-  # validate if the passed file is an image
+  has_secure_password
 
   private
 
